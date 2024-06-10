d3667998f1caf28bc25aca6b38ab92ae3d8a71ee
Vibha Chandrasekar <vibhachandrasekar12345@gmail.com>
2024-06-10 11:33:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Title from './title';
 import AddBook from './addBook';
 import React, { useState, useEffect } from 'react'
-import axios from "axios"
-import { useNavigate } from 'react-router-dom';
+//import axios from "axios"
+//import { useNavigate } from 'react-router-dom';
 
 function App() {
 
   const [data, setData] = useState([{}])
+
+  /*
 
   const navigate = useNavigate();
 
   const handleNavigate = () => {
     navigate('/about');
   };
+
+  */
 
 
 
